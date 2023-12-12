375d244f117c0325660459b1578988b13f89a844
Priya Rawat <85844807+PriyaRawat18@users.noreply.github.com>
2023-12-12 16:02:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") //it'll not run because we're using nodejs, not browser
+
+//code readability should be important
+let name = "Priya"
+let age = 20;
+let isLoggedIn = true
+
+/*
+number range => 2^53
+bigint
+string
+boolean => ture/false
+null => standalone value
+undefined => 
+symbol => unique
+
+object
+*/
+
+console.log(typeof age)         //number
+console.log(typeof undefined)   //undefined
+console.log(typeof null)        //object
